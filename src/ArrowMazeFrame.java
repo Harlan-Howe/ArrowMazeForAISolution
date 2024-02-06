fcbdd637baabc6b134bb448d89bdc2edb67601f5
harlan.howe <harlan.howe@kinkaid.org>
2024-02-06 16:12:01 -0600

    Initial setup of layout and buttons

--- src/ArrowMazeFrame.java
+++ src/ArrowMazeFrame.java
@@ -1,6 +1,46 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ArrowMazeFrame extends JFrame
+public class ArrowMazeFrame extends JFrame implements ActionListener
 {
+    private ArrowMazePanel mazePanel;
+    private JButton resetButton, executeButton;
 
+
+    public ArrowMazeFrame()
+    {
+        super("ArrowMaze");
+        setSize((ArrowCell.LEFT_MARGIN*2+ArrowCell.CELL_SIZE*ArrowMazePanel.NUM_COLS),
+                ArrowCell.TOP_MARGIN*2+ArrowCell.CELL_SIZE*ArrowMazePanel.NUM_ROWS+80);
+        setResizable(false);
+        getContentPane().setLayout(new BorderLayout());
+        mazePanel = new ArrowMazePanel();
+        getContentPane().add(mazePanel, BorderLayout.CENTER);
+        JPanel bottomPanel = new JPanel();
+        resetButton = new JButton("Reset");
+        executeButton =new JButton("Execute");
+        resetButton.addActionListener(this);
+        executeButton.addActionListener(this);
+        bottomPanel.add(resetButton);
+        bottomPanel.add(executeButton);
+        getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent aEvt)
+    {
+        if (aEvt.getSource()==resetButton)
+        {
+            System.out.println("Reset pressed");
+            mazePanel.resetField();
+        }
+        if (aEvt.getSource()==executeButton)
+        {
+            System.out.println("Execute pressed");
+        }
+    }
 }
