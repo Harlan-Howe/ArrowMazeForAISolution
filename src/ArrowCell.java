8d5d74d785c52155b47350a91e415b73fd5a260a
harlan.howe <harlan.howe@kinkaid.org>
2024-02-06 22:42:43 -0600

    Commented drawSelf method. In drawSelf, removed unnecessary cast of g to a Graphics2D Capitalized variable names for ARROWS and ARROW_FONT

--- src/ArrowCell.java
+++ src/ArrowCell.java
@@ -6,6 +6,7 @@ public class ArrowCell
     private int direction;
     private Color myColor;
 
+    // Define constants
     public final static int LEFT = 0;
     public final static int UP = 1;
     public final static int RIGHT = 2;
@@ -15,8 +16,9 @@ public class ArrowCell
     public final static int LEFT_MARGIN = 10;
     public final static int TOP_MARGIN = 10;
 
-    private final static String[] arrows = {"←","↑","→","↓"};
-    private static final Font arrowFont = new Font("Arial",Font.BOLD, CELL_SIZE-4);
+    private final static String[] ARROWS = {"←","↑","→","↓"};
+    private static final Font ARROW_FONT = new Font("Arial",Font.BOLD, CELL_SIZE-4);
+
     public ArrowCell(int myRow, int myCol) {
         this(myRow,myCol, (int)(Math.random()*4));
     }
@@ -54,22 +56,25 @@ public class ArrowCell
         return "ArrowCell{" +
                 "myRow=" + myRow +
                 ", myCol=" + myCol +
-                ", direction=" + direction + " " + arrows[direction] +
+                ", direction=" + direction + " " + ARROWS[direction] +
                 ", myColor=" + myColor +
                 '}';
     }
 
     public void drawSelf(Graphics g)
     {
-
+        // background
         g.setColor(myColor);
         g.fillRect(LEFT_MARGIN + CELL_SIZE*myCol, TOP_MARGIN + CELL_SIZE*myRow, CELL_SIZE, CELL_SIZE);
+        // border
         g.setColor(Color.LIGHT_GRAY);
         g.drawRect(LEFT_MARGIN + CELL_SIZE*myCol, TOP_MARGIN + CELL_SIZE*myRow, CELL_SIZE, CELL_SIZE);
+        // arrow
         g.setColor(Color.WHITE);
-        g.setFont(arrowFont);
-        int arrowWidth = ((Graphics2D)g).getFontMetrics().stringWidth(arrows[direction]);
-        g.drawString(arrows[direction],LEFT_MARGIN + CELL_SIZE * myCol + CELL_SIZE/2 - arrowWidth/2,
-                TOP_MARGIN + CELL_SIZE * (myRow) + CELL_SIZE/2+ 6);
+        g.setFont(ARROW_FONT);
+        int arrowWidth = g.getFontMetrics().stringWidth(ARROWS[direction]); // width of arrow string in
+                                                                                          // this font
+        g.drawString(ARROWS[direction],LEFT_MARGIN + CELL_SIZE * myCol + CELL_SIZE/2 - arrowWidth/2,
+                TOP_MARGIN + CELL_SIZE * (myRow) + CELL_SIZE/2 + 6);
     }
 }
